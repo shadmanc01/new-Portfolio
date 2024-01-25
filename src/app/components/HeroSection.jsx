ae0a2a1f89e1b02d6d7d314e266d3e09778b6b2e
Shadman <shadmanchowdhury01@gmail.com>
2024-01-25 13:03:19 -0500

    changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,18 +17,26 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hey, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Shadman",
                 1000,
-                "Web Developer",
+                "a Frontend Developer",
                 1000,
-                "Mobile Developer",
+                "a Backend Developer",
                 1000,
-                "UI/UX Designer",
+                "a Digital Product Specialist @ IKEA",
+                1000,
+                "a Product Manager",
+                1000,
+                "a UI/UX Designer",
+                1000,
+                "a Poet",
+                1000,
+                "a Poet",
                 1000,
               ]}
               wrapper="span"
@@ -36,9 +44,8 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+          <p style={{padding:'3vh'}}className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+          Business-oriented with the drive to learn and collaborate. My Interests include, yet are not limited to, the arts, creative writing, interior design, web development, and software development. I aspire to simplify the world with one line of code at a time.
           </p>
           <div>
             <Link
@@ -63,7 +70,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div style={{borderRadius: '25vh', overflow: 'hidden'}}className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
