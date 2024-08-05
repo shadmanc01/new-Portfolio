db9b80c844c7aa67907f3fe03dbdd6e63e07051e
Shadman Chowdhury <shadmanchowdhury01@gmail.com>
2024-08-05 15:48:55 -0400

    updated footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>SC</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <p className="text-slate-600">This website was built using Next.JS, feel free to fork my repo :)</p>
       </div>
     </footer>
   );
